a382aac612a7271010133246637330bda40d3cfd
harshit <harshit2k19@gmail.com>
2026-03-13 03:23:12 +0530

    Finished assn 1

--- hw1/cs224r/infrastructure/pytorch_util.py
+++ hw1/cs224r/infrastructure/pytorch_util.py
@@ -1,10 +1,9 @@
-'''
+"""
 TO EDIT: Utilities for handling PyTorch
 
 Functions to edit:
-    1. build_mlp (line 26) 
-'''
-
+    1. build_mlp (line 26)
+"""
 
 from typing import Union
 
@@ -15,39 +14,40 @@ Activation = Union[str, nn.Module]
 
 
 _str_to_activation = {
-    'relu': nn.ReLU(),
-    'tanh': nn.Tanh(),
-    'leaky_relu': nn.LeakyReLU(),
-    'sigmoid': nn.Sigmoid(),
-    'selu': nn.SELU(),
-    'softplus': nn.Softplus(),
-    'identity': nn.Identity(),
+    "relu": nn.ReLU(),
+    "tanh": nn.Tanh(),
+    "leaky_relu": nn.LeakyReLU(),
+    "sigmoid": nn.Sigmoid(),
+    "selu": nn.SELU(),
+    "softplus": nn.Softplus(),
+    "identity": nn.Identity(),
 }
 
 device = None
 
+
 def build_mlp(
-        input_size: int,
-        output_size: int,
-        n_layers: int,
-        size: int,
-        activation: Activation = 'tanh',
-        output_activation: Activation = 'identity',
+    input_size: int,
+    output_size: int,
+    n_layers: int,
+    size: int,
+    activation: Activation = "tanh",
+    output_activation: Activation = "identity",
 ) -> nn.Module:
     """
-        Builds a feedforward neural network
+    Builds a feedforward neural network
 
-        Arguments:
-            n_layers: number of hidden layers
-            size: dimension of each hidden layer
-            activation: activation of each hidden layer
+    Arguments:
+        n_layers: number of hidden layers
+        size: dimension of each hidden layer
+        activation: activation of each hidden layer
 
-            input_size: size of the input layer
-            output_size: size of the output layer
-            output_activation: activation of the output layer
+        input_size: size of the input layer
+        output_size: size of the output layer
+        output_activation: activation of the output layer
 
-        Returns:
-            MLP (nn.Module)
+    Returns:
+        MLP (nn.Module)
     """
     if isinstance(activation, str):
         activation = _str_to_activation[activation]
@@ -56,8 +56,19 @@ def build_mlp(
 
     # TODO: return a MLP. This should be an instance of nn.Module
     # Note: nn.Sequential is an instance of nn.Module.
-    
-    raise NotImplementedError
+    from collections import OrderedDict
+
+    mlp_dict = OrderedDict()
+    mlp_dict["input"] = nn.Linear(in_features=input_size, out_features=size)
+    for idx in range(1, n_layers + 1):
+        mlp_dict[f"{idx}_act"] = activation
+        mlp_dict[f"{idx}_lin"] = nn.Linear(size, size)
+    mlp_dict["final_act"] = activation
+    mlp_dict["final_lin"] = nn.Linear(size, output_size)
+    mlp_dict["output_act"] = output_activation
+
+    return nn.Sequential(mlp_dict)
+
 
 def init_gpu(use_gpu=True, gpu_id=0):
     global device
@@ -81,4 +92,4 @@ def from_numpy(*args, **kwargs):
 
 
 def to_numpy(tensor):
-    return tensor.to('cpu').detach().numpy()
+    return tensor.to("cpu").detach().numpy()
